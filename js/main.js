41c3d4899929c092554665a59a8b27283095e97b
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-02-25 20:20:04 -0500

    center header vertically

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+var win = $(window),
+	header = $('#header');
 
+function centerHeader() {
+	header.css('top', ( win.height() - header.height() ) / 2);
+}
+centerHeader();
+win.resize(centerHeader);
