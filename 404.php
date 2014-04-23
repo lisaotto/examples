244e74a1164884710438de3a21837df7b1a4e8cd
lisaotto <otto.elizabeth@gmail.com>
2014-04-22 22:49:42 -0400

    404 page

---
+++ 404.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Page Not Found</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <?php $url = 'http://localhost/portfolio';?>
+
+        <link rel="stylesheet" href="<?php echo $url; ?>/css/style.css">
+        
+        <script src="<?php echo $url; ?>/js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body class="teasers-open">
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <header>
+
+        <?php include("includes/header.php"); ?>
+        
+        </header>
+
+        <main id="projects">
+            <div class="error outerpadding">
+                <h2>404</h2>
+                <h3>PAGE NOT FOUND</h3>
+                <p>Nobody here but us bunnies.</p>
+                <img src="<?php echo $url; ?>/img/bunnies.gif">
+            </div>
+            
+        </main>
+
+
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="<?php echo $url; ?>/js/plugins.js"></script>
+        <script src="<?php echo $url; ?>/js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
