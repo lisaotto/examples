8d0c3acf188985ecd07e564c77acfe456d62a0ac
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-04-23 23:08:31 -0400

    404

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+$title = 'Page Not Found';
+include('includes/header.php'); ?>
+
+<main id="projects">
+    <div class="error outerpadding">
+        <div class="404-content">
+            <h2>404</h2>
+            <h3>PAGE NOT FOUND</h3>
+            <p>Nobody here but us bunnies.</p>
+            <img src="<?php echo $cf_url; ?>/img/bunnies.gif">
+        </div>
+    </div>
+    
+</main>
+
+<?php include('includes/footer.php'); ?>
