d250228d251045cf9b668589b64243bbe6dd2cc3
lisaotto <otto.elizabeth@gmail.com>
2014-10-08 21:56:59 -0400

    Added new project - underdog

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 $title = 'Lisa Otto';
 include('includes/header.php'); ?>
-
+  <a href="<?php echo $url; ?>/project/underdog" class="project-sample">
+        <div class="overlay">
+            <div><div class="hgroup"><h2>Underdog</h2><h3>User Onboarding &amp; ?????</h3></div></div>
+            <div><p class="icon-arrow-box"></p></div>
+        </div>             
+    </a>
     <a href="<?php echo $url; ?>/project/platform" class="project-sample">
         <div class="overlay">
             <div><div class="hgroup"><h2>Platform</h2><h3>Blog Integration</h3></div></div>
---
+++ project/underdog/index.php
@@ -0,0 +1,58 @@
+<?php
+$title = 'Platform';
+$slug = 'project/platform';
+include('../../includes/header.php'); ?>
+
+    <div class="banner"> 
+        <div class="bannerimg"><img src="<?php echo $url; ?>/img/underdog/underdog-header.jpg"></div>
+        <div class="hgroup"><h3>User Onboarding</h3><h2>UNDERDOG</h2></div>
+    </div>
+    <div class="navigation">
+        <a class="next" href="<?php echo $url; ?>/project/booksatwork"><span class="title">next project</span><span class="icon-arrow-box"></span></a>
+        <div class="bar"></div>
+    </div>
+    <div class="intro clearfix outerpadding">
+        <p class="description">NCTA was looking to integrate the external blog into the recently redesigned website. The NCTA blog covers a wide range of topics from the latest Internet technology to television programming, so the blog had to remain a place where all of the topics could come together cohesively. Part of the challenge was maintaining the structure and theme of the current site while giving the blog a distinct feel.</p>
+        <div class="role">
+        <h4>My Role</h4>
+        <ul>
+            <li>UX</li>
+            <li>UI</li>
+            <li>HTML/CSS support</li>
+        </ul>
+        </div>
+    </div>
+    <div class="content outerpadding clearfix">
+        <div class="clearfix">
+            <img class="threecolumn" src="<?php echo $url; ?>/img/underdog/underdog-onboarding.png">
+        </div>
+        <div class="clearfix">
+            <h3>The process</h3>
+            <p class="threecolumn">Early wireframes laid out three content types — posts, 'news from elsewhere' (curated links from around the web), and industry data stats. The original design gave prominence to featured content.</p>
+        </div>
+         <div class="clearfix">
+            <img class="threecolumn" src="<?php echo $url; ?>/img/underdog/underdog-flowsketch.jpg">
+            <p class="threecolumn">Because the blog was being integrated into an existing site, all of the features had to fit into the existing grid system. </p>
+        </div>
+        <div class="clearfix">
+            <img class="halfcolumn" src="<?php echo $url; ?>/img/underdog/underdog-wireframe.png">
+            <p class="onecolumn floatright last">Because the blog was being integrated into an existing site, all of the features had to fit into the existing grid system. </p>
+        </div>
+        <div class="clearfix">
+            <h3>2. Searching for Candidates</h3>
+            <p class="threecolumn">Early wireframes laid out three content types — posts, 'news from elsewhere' (curated links from around the web), and industry data stats. The original design gave prominence to featured content.</p>
+        </div>
+        <div class="clearfix">
+
+
+
+
+        <a class="visitsite" href="https://www.underdog.io" target="_blank" title="Underdog">VISIT SITE<span class="icon-arrow"></span></a>
+    </div>
+
+    <div class="bottom-navigation">
+        <a class="backup" href="#projects"><span class="title">back up</span><span class="icon-arrow-box"></span></a>
+        <div class="bar"></div>
+    </div>
+
+<?php include('../../includes/footer.php'); ?>
