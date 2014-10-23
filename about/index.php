0f1b0fd6ec4737e7c8dd52710eaa7af277dd099b
lisaotto <otto.elizabeth@gmail.com>
2014-10-23 18:25:52 -0400

    indecisive about the bio

--- about/index.php
+++ about/index.php
@@ -8,7 +8,7 @@ include('../includes/header.php'); ?>
         <div class="vcenter clearfix">
             <img class="halfcolumn" src="<?php echo $url; ?>/img/about/lisa.jpg">
             <div class="halfcolumn last">
-            	<p class="bio">Lisa is currently a UI/UX designer living in New York City. She is interested in design that can find a balance between functional usability and aesthic playfulness. </p>
+            	<p class="bio">Lisa is currently a UI/UX designer living in New York City. She is interested in designing experiences that balance between functional usability and aesthic playfulness.</p>
                 <p>This site was built by hand by Lisa with Javascript (and moral) support from <a href="http://scottdonaldson.net" target="_blank">Scott Donaldson</a>.</p>
             	<p class="iconz"><a href="mailto:otto.elizabeth@gmail.com" class="icon-envelope"></a><a href="http://dribbble.com/lisa_otto" target="_blank" class="icon-dribbble"></a><a href="twitter.com/lisa_otto" class="icon-twitter"></a> </p>
             </div>
