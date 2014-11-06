8fd5f536c0663854b08a4f2c045cbbdc9a05ac44
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-11-05 20:03:57 -0500

    deploy grabs the main JS

--- deploy.py
+++ deploy.py
@@ -50,3 +50,18 @@ for slug in css:
 	k.key = slug
 	k.content_type = 'text/css'
 	k.set_contents_from_string(r.content)
+
+js = [
+	'js/main.js'
+]
+
+for slug in js:
+
+	r = requests.get( url + slug )
+
+	print 'deploying ' + slug
+
+	k = Key(bucket)
+	k.key = slug
+	k.content_type = 'application/x-javascript'
+	k.set_contents_from_string(r.content)
