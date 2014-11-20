5b3d00d8654690617eaf905ee70f6a5df3b9b2ed
lisaotto <otto.elizabeth@gmail.com>
2014-11-20 15:32:22 -0500

    bio & social icons

--- about/index.php
+++ about/index.php
@@ -9,7 +9,7 @@ include('../includes/header.php'); ?>
         <div class="vcenter clearfix">
             <img alt="Lisa Otto looking Chilly in an Urban Landscape" class="halfcolumn" src="<?php echo $url; ?>/img/about/lisa.jpg">
             <div class="halfcolumn last">
-            	<p class="bio">Lisa is currently a UI/UX designer living in New York City. She is interested in designing experiences that balance between functional usability and aesthetic playfulness.</p>
+            	<p class="bio">I'm currently a UI/UX designer living in New York City. I'm interested in designing experiences that balance between functional usability and aesthetic playfulness. I try to keep in mind that pointing out the problem is the easy part, finding a useful solution is the challenge.</p>
             	<p class="bio">Get in touch at <a href="mailto:otto.elizabeth@gmail.com">otto.elizabeth@gmail.com</a>.</p>
                
             </div>
