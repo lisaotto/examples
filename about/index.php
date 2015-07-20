13c05cd769cd858b03d20c61a82a553cf8ad5a44
lisaotto <otto.elizabeth@gmail.com>
2015-07-20 13:59:11 -0400

    more typos

--- about/index.php
+++ about/index.php
@@ -10,7 +10,7 @@ include('../includes/header.php'); ?>
             <img alt="Lisa Otto" class="halfcolumn stroke" src="<?php echo $url; ?>/img/about/lisa_otto.jpg">
             <div class="halfcolumn last">
             	<p class="bio">I'm an interaction designer living in Pittsburgh while I pursue a <a href="http://design.cmu.edu/content/master-design" target="_blank">Master of Design (MDes) in Design for Interactions at Carnegie Mellon</a>. I'm interested in designing experiences that balance between functional usability and aesthetic playfulness. I try to keep in mind that pointing out the problem is the easy part, finding a useful solution is the challenge.</p>
-            	<p class="bio"> Previously, I've worked as the UX lead at <a href="zenolive.com" target="_blank">ZenoRadio</a>, a radio broadcasting start-up and as a freelance designer for a variety of tech companies and small businesses. For a full work history check out <a href="https://www.linkedin.com/in/ottolisa" target="_blank">Linkedin</a>.</p>
+            	<p class="bio"> Previously, I've worked as the UX lead at <a href="http://zenolive.com" target="_blank">ZenoRadio</a>, a radio broadcasting start-up, and as a freelance designer for a variety of tech companies and small businesses. For a full work history check out <a href="https://www.linkedin.com/in/ottolisa" target="_blank">Linkedin</a>.</p>
             	<p class="bio">Get in touch at <a href="mailto:otto.elizabeth@gmail.com">otto.elizabeth@gmail.com</a>.</p>
             	<h6>Photo by <a href="http://uniquelapin.com" target="_blank">UniqueLapin</h6>
             </div>
