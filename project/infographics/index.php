86e0422d823f757689ac2a6a2d6ccce5675f4f7e
lisaotto <otto.elizabeth@gmail.com>
2015-11-22 21:53:19 -0500

    adding in comm video

--- project/infographics/index.php
+++ project/infographics/index.php
@@ -15,21 +15,30 @@ include('../../includes/header.php'); ?>
         <div class="role">
             <h4>My Role</h4>
             <p>I worked closely with in-house content team to determine concept – provided sketches and final illustrations in a variety of formats. </p>
-            <p>Work done with <a href="http://www.ncta.com" target="_blank" alt="National Cable and Telecommunications Association">NCTA</a></p>
+            <p>Work done at Carnegie Mellon and with<a href="http://www.ncta.com" target="_blank" alt="National Cable and Telecommunications Association"> NCTA</a></p>
         </div>
     </div>
-    <div class="content outerpadding finalblock clearfix">
-        <img alt="Unlicensed Spectrum Infographic" class="threecolumn first" src="<?php echo $url; ?>/img/infographics/SpectrumMap-Full.jpg">
-        <img alt="Coffee Shop Close-Up from Unlicensed Spectrum Infographic" class="halfcolumn" src="<?php echo $url; ?>/img/infographics/SpectrumMap-zoom1.jpg">
-        <img alt="Apartment Bulding Close-Up from Unlicensed Spectrum Infographic" class="halfcolumn last" src="<?php echo $url; ?>/img/infographics/SpectrumMap-zoom2.jpg">
-        <img alt="Real-Time Entertainment Infographic" class="twocolumn center" src="<?php echo $url; ?>/img/infographics/realtimeentertainment.png">
-        <img alt="Teens Use of the Internet Infographic" class="threecolumn" src="<?php echo $url; ?>/img/infographics/schoolsupplies.jpg">
-        <img alt="First Half of the Million Years of Television Infographic" class="threecolumn" src="<?php echo $url; ?>/img/infographics/millionyears-Full1.jpg">
-        <img alt="Second Half of the Million Years of Television Infographic" class="threecolumn" src="<?php echo $url; ?>/img/infographics/millionyears-Full2.jpg">
-        <img alt="Tiger and Printing Press Close-up from the Million Years of Television Infographic" class="halfcolumn" src="<?php echo $url; ?>/img/infographics/millionyears-zoom1.png">
-        <img alt="Smart Car Close-up from the Million Years of Television Infographic" class="halfcolumn last" src="<?php echo $url; ?>/img/infographics/millionyears-zoom2.png">
-        <img alt="Evolution Close-up from the Million Years of Television Infographic" class="halfcolumn" src="<?php echo $url; ?>/img/infographics/millionyears-zoom3.png">
-        <img alt="Cave Painting Close-up from the Million Years of Television Infographic" class="halfcolumn finalblock last" src="<?php echo $url; ?>/img/infographics/millionyears-zoom4.png">
+
+ 
+    <div class="content outerpadding clearfix">
+    
+        <div class="finalblock clearfix">
+            <div class="threecolumn first">
+                <iframe class="iframe-center" src="https://player.vimeo.com/video/144577373?title=0&byline=0&portrait=0" width="600" height="338" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                <a class="intext-link" href="https://medium.com/communication-design-studio/how-can-you-hear-without-ears-5b60c75a7756#.yf3w9xyum" target="_blank"><h3>Full Process Blog</h3></a>
+            </div>
+            <img alt="Unlicensed Spectrum Infographic" class="threecolumn " src="<?php echo $url; ?>/img/infographics/SpectrumMap-Full.jpg">
+            <img alt="Coffee Shop Close-Up from Unlicensed Spectrum Infographic" class="halfcolumn" src="<?php echo $url; ?>/img/infographics/SpectrumMap-zoom1.jpg">
+            <img alt="Apartment Bulding Close-Up from Unlicensed Spectrum Infographic" class="halfcolumn last" src="<?php echo $url; ?>/img/infographics/SpectrumMap-zoom2.jpg">
+            <img alt="Real-Time Entertainment Infographic" class="twocolumn center" src="<?php echo $url; ?>/img/infographics/realtimeentertainment.png">
+            <img alt="Teens Use of the Internet Infographic" class="threecolumn" src="<?php echo $url; ?>/img/infographics/schoolsupplies.jpg">
+            <img alt="First Half of the Million Years of Television Infographic" class="threecolumn" src="<?php echo $url; ?>/img/infographics/millionyears-Full1.jpg">
+            <img alt="Second Half of the Million Years of Television Infographic" class="threecolumn" src="<?php echo $url; ?>/img/infographics/millionyears-Full2.jpg">
+            <img alt="Tiger and Printing Press Close-up from the Million Years of Television Infographic" class="halfcolumn" src="<?php echo $url; ?>/img/infographics/millionyears-zoom1.png">
+            <img alt="Smart Car Close-up from the Million Years of Television Infographic" class="halfcolumn last" src="<?php echo $url; ?>/img/infographics/millionyears-zoom2.png">
+            <img alt="Evolution Close-up from the Million Years of Television Infographic" class="halfcolumn" src="<?php echo $url; ?>/img/infographics/millionyears-zoom3.png">
+            <img alt="Cave Painting Close-up from the Million Years of Television Infographic" class="halfcolumn finalblock last" src="<?php echo $url; ?>/img/infographics/millionyears-zoom4.png">
+        </div>
     </div>
 
     <div class="bottom-navigation">
