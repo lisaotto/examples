6eb87ab5c40e702e521e3626141ddf190bb98a8a
lisaotto <otto.elizabeth@gmail.com>
2016-02-10 21:55:48 -0500

    adding resume

--- about/index.php
+++ about/index.php
@@ -10,7 +10,7 @@ include('../includes/header.php'); ?>
             <img alt="Lisa Otto" class="halfcolumn stroke" src="<?php echo $url; ?>/img/about/lisa_otto.jpg">
             <div class="halfcolumn last">
             	<p class="bio">I'm an interaction designer living in Pittsburgh while I pursue a <a href="http://design.cmu.edu/content/master-design" target="_blank">Master of Design (MDes) in Design for Interactions at Carnegie Mellon</a>. I am interested in asking questions and finding new frames for complex problems. I am particularly oriented toward service design and am looking for a role to play in an organization who understands interactions beyond screens.</p>
-            	<p class="bio"> Previously, I've worked as the UX lead at <a href="http://zenolive.com" target="_blank">ZenoRadio</a>, a radio broadcasting start-up, and as a freelance designer for a variety of tech companies and small businesses. For a full work history check out <a href="https://www.linkedin.com/in/ottolisa" target="_blank">LinkedIn</a>.</p>
+            	<p class="bio"> Previously, I've worked as the UX lead at <a href="http://zenolive.com" target="_blank">ZenoRadio</a>, a radio broadcasting start-up, and as a freelance designer for a variety of tech companies and small businesses. For a full work history check out <a href="<?php echo $url; ?>/otto_resume.pdf" target="_blank">Resume</a>.</p>
             	<p class="bio">Get in touch at <a href="mailto:otto.elizabeth@gmail.com">otto.elizabeth@gmail.com</a>.</p>
             	<p class="small">Photo by <a href="http://uniquelapin.com" target="_blank">UniqueLapin</p>
             </div>
